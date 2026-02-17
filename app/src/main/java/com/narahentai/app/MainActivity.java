f3b3c6df7263cfc88d3323595f26d1184aa59015
viralyy <narablewer@gmail.com>
2026-02-17 20:47:09 +0700

    Update MainActivity.java

--- app/src/main/java/com/narahentai/app/MainActivity.java
+++ app/src/main/java/com/narahentai/app/MainActivity.java
@@ -1,54 +1,79 @@
 package com.narahentai.app;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNav;
+    private MaterialToolbar topBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNav = findViewById(R.id.bottomNav);
+        topBar = findViewById(R.id.topBar);
+        setSupportActionBar(topBar);
+
+        // Hilangin tombol refresh (poin 4) => kita gak pake menu refresh sama sekali
+        // Kita cuma pake icon search dari menu.
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
 
         // default: Home
         if (savedInstanceState == null) {
-            switchTo(new HomeFragment());
+            switchFragment(new HomeFragment(), "Beranda");
+            bottomNav.setSelectedItemId(R.id.nav_home);
         }
 
         bottomNav.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
             if (id == R.id.nav_home) {
-                switchTo(new HomeFragment());
+                switchFragment(new HomeFragment(), "Beranda");
                 return true;
             } else if (id == R.id.nav_update) {
-                // TODO: fragment update lu
-                switchTo(new HomeFragment()); // sementara
+                switchFragment(new UpdateFragment(), "Update");
                 return true;
             } else if (id == R.id.nav_history) {
-                // TODO
-                switchTo(new HomeFragment()); // sementara
+                switchFragment(new HistoryFragment(), "Riwayat");
                 return true;
             } else if (id == R.id.nav_profile) {
-                // TODO
-                switchTo(new HomeFragment()); // sementara
+                switchFragment(new ProfileFragment(), "Profil");
                 return true;
             }
             return false;
         });
     }
 
-    private void switchTo(Fragment f) {
+    private void switchFragment(Fragment fragment, String title) {
+        topBar.setTitle("Narahentai");
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.fragmentContainer, f)
+                .replace(R.id.container, fragment)
                 .commit();
+    }
+
+    // Search aktif (poin 3)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
+            startActivity(new Intent(this, SearchActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
