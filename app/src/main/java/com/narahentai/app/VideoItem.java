cfe9379f01b8955598a9de6aab534fb3169886e0
viralyy <narablewer@gmail.com>
2026-02-18 03:13:13 +0700

    Update VideoItem.java

--- app/src/main/java/com/narahentai/app/VideoItem.java
+++ app/src/main/java/com/narahentai/app/VideoItem.java
@@ -1,10 +1,22 @@
 package com.narahentai.app;
 
 public class VideoItem {
-    public String title;
-    public String thumbnail_url;
-    public String video_url;
-    public int views;
-    public int duration_minutes;
-    public String created_at;
+    public final String title;
+    public final String thumbnailUrl;
+    public final String videoUrl;
+    public final int durationMinutes;
+    public final int views;
+
+    public VideoItem(String title, String thumbnailUrl, String videoUrl, int durationMinutes, int views) {
+        this.title = title;
+        this.thumbnailUrl = thumbnailUrl;
+        this.videoUrl = videoUrl;
+        this.durationMinutes = durationMinutes;
+        this.views = views;
+    }
+
+    public String durationText() {
+        // lu bisa bikin lebih detail nanti (mm:ss)
+        return durationMinutes + " menit";
+    }
 }
