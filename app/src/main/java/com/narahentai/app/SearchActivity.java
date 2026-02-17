0436b0c9dadacbf5761d8cb422072061a65773f7
viralyy <narablewer@gmail.com>
2026-02-17 22:48:11 +0700

    Create SearchActivity.java

---
+++ app/src/main/java/com/narahentai/app/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.narahentai.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
