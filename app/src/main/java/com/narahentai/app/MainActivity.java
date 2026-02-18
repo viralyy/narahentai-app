07146a431cdeda19324f1417cea06c64e68bbd36
viralyy <narablewer@gmail.com>
2026-02-18 15:34:32 +0700

    Update MainActivity.java

--- app/src/main/java/com/narahentai/app/MainActivity.java
+++ app/src/main/java/com/narahentai/app/MainActivity.java
@@ -1,16 +1,16 @@
 package com.narahentai.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         TextView tv = new TextView(this);
-        tv.setText("Narahentai ✅ (minimal mode)");
+        tv.setText("Narahentai ✅ (super minimal, no AndroidX)");
         tv.setTextSize(18);
         tv.setPadding(40, 80, 40, 40);
         setContentView(tv);
