860507143fb5111a408cf751a3b1fb0283b344d0
viralyy <narablewer@gmail.com>
2026-02-18 15:18:59 +0700

    Update SearchActivity.java

--- app/src/main/java/com/narahentai/app/SearchActivity.java
+++ app/src/main/java/com/narahentai/app/SearchActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
