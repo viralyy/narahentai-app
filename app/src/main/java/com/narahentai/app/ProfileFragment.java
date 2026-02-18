847e06e94514d5acf7d5895e56fee84b46ced86e
viralyy <narablewer@gmail.com>
2026-02-18 15:18:36 +0700

    Update ProfileFragment.java

--- app/src/main/java/com/narahentai/app/ProfileFragment.java
+++ app/src/main/java/com/narahentai/app/ProfileFragment.java
@@ -2,7 +2,6 @@ package com.narahentai.app;
 
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -10,11 +9,11 @@ import androidx.fragment.app.Fragment;
 public class ProfileFragment extends Fragment {
 
     public ProfileFragment() {
-        super(R.layout.fragment_simple_page);
+        super(R.layout.fragment_profile);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        // kosong dulu
+        // placeholder
     }
 }
