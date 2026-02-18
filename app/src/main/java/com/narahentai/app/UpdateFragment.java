d2f4b6e0caf14fb7d118e6037eb70dd6d85a40b2
viralyy <narablewer@gmail.com>
2026-02-18 15:18:04 +0700

    Update UpdateFragment.java

--- app/src/main/java/com/narahentai/app/UpdateFragment.java
+++ app/src/main/java/com/narahentai/app/UpdateFragment.java
@@ -2,7 +2,6 @@ package com.narahentai.app;
 
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -10,11 +9,11 @@ import androidx.fragment.app.Fragment;
 public class UpdateFragment extends Fragment {
 
     public UpdateFragment() {
-        super(R.layout.fragment_simple_page);
+        super(R.layout.fragment_update);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        // kosong dulu
+        // placeholder
     }
 }
